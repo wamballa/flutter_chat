f910c9f2baced3f2b14036f6b6007495c3858689
wamballa <steve.tyrell@gmail.com>
2021-08-31 22:21:14 +0100

    update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 const kSendButtonTextStyle = TextStyle(
@@ -15,5 +17,23 @@ const kMessageTextFieldDecoration = InputDecoration(
 const kMessageContainerDecoration = BoxDecoration(
   border: Border(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+  ),
+);
+
+const kInputDecorationHintText = InputDecoration(
+  focusColor: Colors.black,
+  hintText: '',
+  hintStyle: TextStyle(color: Colors.grey),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
 );
